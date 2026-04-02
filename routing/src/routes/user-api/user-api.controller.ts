0ecf484b66b205abbb7b7a9b4c341d304de7daf4
corpjorge <corpjorge@hotmail.com>
2026-04-02 14:54:35 -0500

    feat: add routing

--- routing/src/routes/user-api/user-api.controller.ts
+++ routing/src/routes/user-api/user-api.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserApiService } from './user-api.service';
+import {Body, Controller, Post} from '@nestjs/common';
+import {UserApiService} from './user-api.service';
 
 @Controller('/users-api/v1/')
 export class UserApiController {
@@ -7,8 +7,6 @@ export class UserApiController {
 
   @Post('/registration')
   async sendUserData(@Body() data: unknown): Promise<unknown> {
-    const res = await this.service.sendUserData(data);
-    console.log(res);
-    return res;
+    return await this.service.sendUserData(data);
   }
 }
